9f1afca576240c775e92fa1f242bc79bd1bae7df
huyongfeng <huyf@anpro-tech.com>
2025-08-15 11:03:44 +0800

    refactor(main): 重构主程序并添加 Ubuntu 数据收集

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
+from alpine_collector import collect_alpine
 from deb_collector import collect_deb
 
 if __name__ == "__main__":
-    parallel_mode = False
-    parallel_num = 4
     source_file_save = False
     http_timeout = 60
     alpine_dir = "downloads/alpine"
     debian_dir = "downloads/debian"
+    ubuntu_dir = "downloads/ubuntu"
 
-    # collect_alpine(output_dir=alpine_dir, parallel=parallel_mode, save=source_file_save,workers=parallel_num)
+    collect_alpine(output_dir=alpine_dir, save=source_file_save)
     collect_deb(base_url="https://mirrors.aliyun.com/debian/", output_dir=debian_dir, type_name="Debian",
-                parallel=parallel_mode,
-                timeout=http_timeout, workers=parallel_num)
+                timeout=http_timeout)
+    collect_deb(base_url="https://mirrors.aliyun.com/ubuntu/", output_dir=debian_dir, type_name="Ubuntu",
+                timeout=http_timeout)
